4e390371e0ceaae6f707e132637de145107fe65d
jakob.derzapf <Jakob.D.Baron@outlook.de>
2024-11-19 12:01:08 +0100

    PasswordHelper SHA256 Verschlüsselung

---
+++ PasswordHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace UserManagementApp.Utils
+{
+    public static class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256.ComputeHash(passwordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+    }
+}
